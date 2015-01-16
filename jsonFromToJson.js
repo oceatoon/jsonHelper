8cbe2b270ab53be3f7546eb73bb2083864cfced7
Tibor Katelbach <oceatoon@gmail.com>
2015-01-16 16:41:35 +0400

    Update jsonFromToJson.js

--- jsonFromToJson.js
+++ jsonFromToJson.js
@@ -1,7 +1,6 @@
-
 var jsonFromToJson = {
 
-  "fromjson" : [
+  "fromJson" : [
     {"x":1, "y": {"c" : 20, "o" : 30} },
     {"x":2, "y": {"c" : 60, "o" : 50} }
   ],
@@ -9,31 +8,40 @@ var jsonFromToJson = {
           "a" : "x" , 
           "b" : function(obj){ return obj.y.c + obj.y.o }
           },
-  "outputLine" : {"a":"", "b":""},
   "toJson" : [],
   
   "test" : function(){
-  	console.log("test");  
+  	console.log("test from ",this.fromJson);  
     this.convert();
+    console.log("test to ",this.toJson);  
   },
   
   "convert" : function(){
-  	console.log("convert");     
-  	this.toJson = [];
-    $.each(this.fromJson,function(i, fromObj){
+  	console.log("convert from",this.fromJson,this.toJson);     
+  	var toJson = [];
+  	var rules = this.rules;
+  	
+    $(this.fromJson).each(function(i, fromObj){
 
-	    newLine = this.outputLine;
-	    $.each(this.rules,function(keyTo, convertTo){
+	    newLine = {};
+	    console.log("convert new Line before",newLine);   
+	    $.each(rules, function(keyTo, convertTo){
 	   		console.log("convert rules ",fromObj,keyTo,convertTo);     
 	        if(typeof convertTo == "function")
 	        	newLine[ keyTo ] = convertTo( fromObj );
 	        else
 	        	newLine[ keyTo ] = fromObj[convertTo];
 	    });
-	      
-	    this.toJson.push(newLine);
+	    console.log("convert new Line after",newLine);   
+	    toJson.push(newLine);
     });
+    console.log("convert to",toJson); 
+    this.toJson = toJson;
+  },
+
+  "getTo" : function(){
+  	console.log("getTo");      
     return this.toJson;
-  }
+  },
 
 }
