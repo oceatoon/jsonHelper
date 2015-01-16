768947f32e15f155489c695a132c6e592880312a
Tibor Katelbach <oceatoon@gmail.com>
2015-01-16 16:59:52 +0400

    Update jsonFromToJson.js

--- jsonFromToJson.js
+++ jsonFromToJson.js
@@ -11,36 +11,33 @@ var jsonFromToJson = {
   "toJson" : [],
   
   "test" : function(){
-  	console.log("test from ",this.fromJson);  
-    this.convert();
-    console.log("test to ",this.toJson);  
+    this.convert(); 
   },
   
   "convert" : function(){
-  	console.log("convert from",this.fromJson,this.toJson);     
+  	console.log("from",this.fromJson);     
   	var toJson = [];
   	var rules = this.rules;
-  	
+  	console.log("rules",rules);
     $(this.fromJson).each(function(i, fromObj){
 
 	    newLine = {};
-	    console.log("convert new Line before",newLine);   
+	    //console.log("convert new Line before",newLine);   
 	    $.each(rules, function(keyTo, convertTo){
-	   		console.log("convert rules ",fromObj,keyTo,convertTo);     
+	   		//console.log("convert rules ",fromObj,keyTo,convertTo);     
 	        if(typeof convertTo == "function")
 	        	newLine[ keyTo ] = convertTo( fromObj );
 	        else
 	        	newLine[ keyTo ] = fromObj[convertTo];
 	    });
-	    console.log("convert new Line after",newLine);   
+	    console.log("converted line data ",newLine);   
 	    toJson.push(newLine);
     });
-    console.log("convert to",toJson); 
+    console.log("to",toJson); 
     this.toJson = toJson;
   },
 
-  "getTo" : function(){
-  	console.log("getTo");      
+  "getTo" : function(){    
     return this.toJson;
   },
 
