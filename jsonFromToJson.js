5f9eacdd5188988a436b2bf7ffba9910bedcd44a
Robert Johnson <zeriam974@gmail.com>
2015-01-22 13:21:50 +0400

    modification JSON helper

--- jsonFromToJson.js
+++ jsonFromToJson.js
@@ -1,44 +1,69 @@
 var jsonFromToJson = {
 
-  "fromJson" : [
-    {"x":1, "y": {"c" : 20, "o" : 30} },
-    {"x":2, "y": {"c" : 60, "o" : 50} }
-  ],
-  "rules" : { 
-          "a" : "x" , 
-          "b" : function(obj){ return obj.y.c + obj.y.o }
-          },
-  "toJson" : [],
-  
-  "test" : function(){
-    this.convert(); 
-  },
-  
-  "convert" : function(){
-  	console.log("from",this.fromJson);     
-  	var toJson = [];
-  	var rules = this.rules;
-  	console.log("rules",rules);
-    $(this.fromJson).each(function(i, fromObj){
+      "fromJson" : [
+      ],
+      "rules" : { 
+              }
+      "toJson" : [],
+      
+      "test" : function(){
+        this.convertObject(); 
+      },
+      
+      "convertArray" : function(){
+        //console.log("from",this.fromJson);
 
-	    newLine = {};
-	    //console.log("convert new Line before",newLine);   
-	    $.each(rules, function(keyTo, convertTo){
-	   		//console.log("convert rules ",fromObj,keyTo,convertTo);     
-	        if(typeof convertTo == "function")
-	        	newLine[ keyTo ] = convertTo( fromObj );
-	        else
-	        	newLine[ keyTo ] = fromObj[convertTo];
-	    });
-	    console.log("converted line data ",newLine);   
-	    toJson.push(newLine);
-    });
-    console.log("to",toJson); 
-    this.toJson = toJson;
-  },
+        var toJson = [];
+        var rules = this.rules;
+        //console.log("rules",rules);
+        $(this.fromJson).each(function(i, fromObj){
 
-  "getTo" : function(){    
-    return this.toJson;
-  },
+          newLine = {};
+          //console.log("convert new Line before",newLine, "i", i);   
+          $.each(rules, function(keyTo, convertTo){
+            //console.log("convert rules ",fromObj,keyTo,convertTo);     
+              if(typeof convertTo == "function")
+                newLine[ keyTo ] = convertTo( fromObj );
+              else
+                newLine[ keyTo ] = fromObj[convertTo];
+          });
+          console.log("converted line data ",newLine);   
+          toJson.push(newLine);
+        });
+        //console.log("to",toJson); 
+        this.toJson = toJson;
+      },
 
-}
+
+      "convertObject" : function(){
+        //console.log("from",this.fromJson);
+
+        var toJson = {};
+        var rules = this.rules;
+        console.log("rules",rules);
+        $.each(this.fromJson,function(key, fromObj){
+
+          newLine = {};
+          //console.log("convert new Line before key", key);   
+          $.each(rules, function(keyTo, convertTo){
+            //console.log("convert rules ",fromObj,key,convertTo);     
+              if(typeof convertTo == "function")
+                newLine[ keyTo ] = convertTo( fromObj );
+              else
+                newLine[ keyTo ] = fromObj[convertTo];
+          });
+          //console.log("converted line data ",newLine);
+          if(!toJson[key])
+              toJson[key] = {};
+          toJson[key] = newLine;
+        });
+        console.log("to",toJson); 
+        this.toJson = toJson;
+      },
+
+
+      "getTo" : function(){    
+        return this.toJson;
+      },
+
+    }
